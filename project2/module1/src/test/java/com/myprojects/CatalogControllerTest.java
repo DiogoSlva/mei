4f4dbc051827ed1689f23485ad4bf7ea64b7dd4e
Diogo <86209061+DiogoSlva@users.noreply.github.com>
2024-10-25 14:12:17 +0100

    catalag test

--- project2/module1/src/test/java/com/myprojects/CatalogControllerTest.java
+++ project2/module1/src/test/java/com/myprojects/CatalogControllerTest.java
@@ -1,11 +1,46 @@
 package com.myprojects;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import com.myprojects.enums.Category;
+import com.myprojects.models.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import java.util.Arrays;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class CatalogControllerTest {
+
     @Autowired
     private MockMvc mvc;
+
+    @Test
+    public void getProduct() throws Exception{
+        mvc.perform(MockMvcRequestBuilders.get("/catalog/products/P002").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code"). value("P002"));
+    }
+
+    @Test
+    public void getProducts_category() throws Exception{
+
+        mvc.perform(MockMvcRequestBuilders.get("/catalog/products-by-category/PRINTERS").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$[0].price").isNumber()
+                .andExpect(jsonPath(("$."))
+    }
 
 
 }
